51586ca518447d32536168b72ea82bd17b904221
Priya Sodhi <priya.sodhi@xero.com>
2021-09-29 11:37:21 +1300

    Add the static plant data

---
+++ PlantData.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: "1",
+    plantName: "monstera",
+    description: "The swiss cheese plant produce bright, glossy leaves and makes a popular house plant. It is native to tropical forest regions in Central America.",
+    botanicalName: "Monstera deliciosa",
+    water: "Every 5 days",
+    fertilization: "Every 30 days",
+    careGuide: {
+      soil: "chalky, loam, clay, sand well-drained",
+      sun: "Partial sun",
+      difficultyRating: "super easy",
+      plantingTime: "spring, summer, autumn",
+      potting: "Needs excellent drainage in pots",
+      pruning: "Trim the dead, diseased, overgrown branches in winter"
+    }
+  },
+  {
+    id: "2",
+    plantName: "bird_of_paradise",
+    description: "Giant white bird of paradise is a tropical tree that will grown 8-9 m tall. It has banana-plant type leaves and a palm tree like trunk.",
+    botanicalName: "Strelitzia nicolai",
+    water: "Every 3 days",
+    fertilization: "Every 30 days",
+    careGuide: {
+      soil: "rich soil, well-drained",
+      sun: "full sun",
+      difficultyRating: "medium",
+      plantingTime: "spring",
+      potting: "Needs excellent drainage in pots",
+      pruning: "Trim the dead, diseased, overgrown branches in winter"
+    }
+  }
+]
