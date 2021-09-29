c2e78d3e8ebc5f2919debba4e17fd23511368949
Priya Sodhi <priya.sodhi@xero.com>
2021-09-29 17:17:09 +1300

    Moved the React components in a seperate file

---
+++ src/components/PlantInfo.js
@@ -0,0 +1,59 @@
+﻿function CareGuide({sun, soil, difficultyRating, plantingTime, potting, pruning}) {
+    return(
+        <span className="careGuide w-100 h-100">
+            <strong>Sun: </strong>{sun}
+            <strong>Soil: </strong>{soil}
+            <strong>Difficulty Rating: </strong>{difficultyRating}
+            <strong>Planting Time: </strong>{plantingTime}
+            <strong>Potting: </strong> {potting}
+            <strong>Pruning: </strong> {pruning}
+        </span>
+    )
+}
+
+function PlantImage({plantName, botanicalName}) {
+    return(
+        <div className="plant-img d-flex flex-row justify-content-center align-items-center h-300">
+            <img
+                className="contain-fit"
+                src={`/images/${plantName}.jpg`}
+                height="200"
+                alt={`${{botanicalName}}`}
+            />
+        </div>
+    )
+}
+
+function PlantDemographics({botanicalName, description, water, fertilization }) {
+    return (
+        <div className="plant-info">
+            <div className="d-flex justify-content-between mb-3">
+                <h3 className="text-truncate w-200">
+                    {botanicalName}
+                </h3>
+            </div>
+            <div>
+                <p>
+                    {description}{water}{fertilization}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+function Plant({plantInfo}) {
+    const{plantName, botanicalName, careGuide} = plantInfo;
+    return(
+        <div className="col-xs-12 col-sm-12 col-md-16 col-lg-4 col-sm-12 col-xs-12">
+            <div className="card card-height p-3 mt-3">
+                <PlantImage plantName = {plantName} botanicalName={botanicalName}/>
+                <PlantDemographics {...plantInfo} />
+                <div className="careGuideBox card h-30">
+                    <CareGuide {...careGuide}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Plant;
