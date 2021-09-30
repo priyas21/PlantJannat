83c4c5759fcb66d194b8be757d3638c4a0d86b40
Priya Sodhi <priya.sodhi@xero.com>
2021-09-30 21:07:23 +1300

    Add Toolbar showing the care guide and theme for the page

--- src/components/AllPlants.js
+++ src/components/AllPlants.js
@@ -1,11 +1,13 @@
 ﻿import {data} from "../../PlantData";
 import PlantsList from "./PlantsList";
 import Header from "./Header";
+import PlantToolbar from "./PlantToolbar"
 
 function AllPlants() {
     return(
         <div className="container-fluid">
             <Header />
+            <PlantToolbar />
             <PlantsList data ={data} />
         </div>
     )
---
+++ src/components/PlantToolbar.js
@@ -0,0 +1,33 @@
+﻿function PlantToolbar() {
+    return(
+        <div className="toolbar dark-theme-header">
+            <div className="container">
+                <div className="justify-content-between">
+                    <ul className="toolrow d-flex flex-column flex-lg-row">
+                        <li className="d-flex flex-column flex-md-row">
+                            <b>Show Care Guide&nbsp;&nbsp;</b>
+                            <label className="fav">
+                                <input
+                                type="checkbox"
+                                checked={true}
+                                />
+                                <span className="switch"></span>
+                            </label>
+                        </li>
+                        <li className="d-flex flex-column flex-md-row ml-sm-5 ml-0">
+                            <strong>Theme</strong>
+                            <label className="dropdown">
+                                <select className="form-control theme" value="light">
+                                    <option value="light">Light</option>
+                                    <option value="dark">Dark</option>
+                                </select>
+                            </label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PlantToolbar;
