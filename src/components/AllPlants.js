563966f723d35868b999d035d6ff6e0f29443873
Priya Sodhi <priya.sodhi@xero.com>
2021-09-30 16:37:16 +1300

    Add Header component

--- src/components/AllPlants.js
+++ src/components/AllPlants.js
@@ -1,9 +1,11 @@
 ﻿import {data} from "../../PlantData";
 import PlantsList from "./PlantsList";
+import Header from "./Header";
 
 function AllPlants() {
     return(
         <div className="container-fluid">
+            <Header />
             <PlantsList data ={data} />
         </div>
     )
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+﻿function Header() {
+    return(
+        <div className="padT4 padB4">
+            <div className="container mobile-container">
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <img
+                            alt="Plant Jannat Home page"
+                            src="/images/logo.jpg"
+                            width="50"
+                            height="50"
+                        />
+                    </div>
+                    <div className="light">
+                        <h4 className="header-title">
+                            Plant Jannat
+                        </h4>
+                    </div>
+                    <div className="text-dark">
+                        Hello! Priya &nbsp;
+                        <span>
+                            <a href="#">sign-out</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Header;
