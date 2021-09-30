1f658c40a0c51f114acc60bf75942f8c12471e88
Priya Sodhi <priya.sodhi@xero.com>
2021-09-30 22:03:12 +1300

    Add more plant data

--- PlantData.js
+++ PlantData.js
@@ -2,11 +2,11 @@ export const data = [
   {
     id: "1",
     plantName: "monstera",
-    description: "The swiss cheese plant produce bright, glossy leaves and makes a popular house plant. It is native to tropical forest regions in Central America.",
+    description: "The swiss cheese plant produce bright, glossy leaves and makes a popular house plant. Native to tropical forest regions in Central America.",
     botanicalName: "Monstera deliciosa",
     water: "Every 5 days",
     fertilization: "Every 30 days",
-    soil: "chalky, loam, clay, sand well-drained",
+    soil: "chalky, loam, sand well-drained",
     sun: "Partial sun",
     careGuide: {
       difficultyRating: "super easy",
@@ -23,12 +23,60 @@ export const data = [
     water: "Every 3 days",
     fertilization: "Every 30 days",
     soil: "Rich soil, well-drained",
-    sun: "full sun",
+    sun: "Full sun",
     careGuide: {       
       difficultyRating: "medium",
       plantingTime: "spring",
       potting: "Needs excellent drainage in pots",
       pruning: "Trim the dead, diseased, overgrown branches in winter"
+    }
+  },
+  {
+    id: "3",
+    plantName: "fiddle_leaf",
+    description: "It has leaves that are shaped like a violin. It makes an architectural statement with its unique and lush leaves.",
+    botanicalName: "Ficus lyrata",
+    water: "Every 7 days",
+    fertilization: "Every 180 days",
+    soil: "Slightly acidic, well-drained",
+    sun: "Full sun to partial sun",
+    careGuide: {
+      difficultyRating: "easy",
+      plantingTime: "spring, summer, autumn",
+      potting: "Needs excellent drainage in pots",
+      pruning: "Trim the dead, diseased, overgrown branches in winter"
+    }
+  },
+  {
+    id: "4",
+    plantName: "heart_leaf",
+    description: "It is an evergreen climbing plant originating from the West Indies. It is famous for it glossy, heart-shaped leaves.",
+    botanicalName: "Philondendron hedraceum",
+    water: "Every 7 days",
+    fertilization: "Every 30 days",
+    soil: "Sand, loam",
+    sun: "Partial sun to full shade",
+    careGuide: {
+      difficultyRating: "easy",
+      plantingTime: "spring, autumn",
+      potting: "Needs excellent drainage in pots",
+      pruning: "Trim diseased, withered leaves once a month."
+    }
+  },
+  {
+    id: "5",
+    plantName: "lemon",
+    description: "It is cultivated small evergreen tree known for its yellow, juicy, sour fruits full of citric acid. ",
+    botanicalName: "Citrus lemon",
+    water: "Every 7 days",
+    fertilization: "Every 180 days",
+    soil: "Slightly acidic, well-drained",
+    sun: "Full sun",
+    careGuide: {
+      difficultyRating: "easy",
+      plantingTime: "spring, autumn",
+      potting: "Needs excellent drainage in pots",
+      pruning: "Shape the plant every 2 months."
     }
   }
 ]
