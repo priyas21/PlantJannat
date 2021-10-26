54d7c74ed48493e44cf17cba82988428411ea3cb
Priya Sodhi <priya.sodhi@xero.com>
2021-10-26 14:33:42 +1300

    Add Loading and error state to Plant list component. Use ReactPlaceholder when loading state is true

--- src/components/PlantsList.js
+++ src/components/PlantsList.js
@@ -1,15 +1,28 @@
 ﻿import Plant from "./PlantInfo";
 import {data} from "../../PlantData";
 import {useEffect, useState} from "react";
+import ReactPlaceholder from "react-placeholder";
 
 function PlantsList({showCareGuide}) {
     const[plantsData, setPlantsData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasErrored, setHasErrored] = useState(false);
+    const [error, setError] = useState("");
+    
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     
     useEffect(() => {
         async function delayFunc() {
-            await delay(2000);
-            setPlantsData(data)
+            try {
+                await delay(2000);
+                setIsLoading(false);
+                setPlantsData(data)
+            }
+            catch (e) {
+                setIsLoading(false);
+                setHasErrored(true);
+                setError(e);
+            }
         }
         delayFunc();
     },[]);
@@ -31,20 +44,35 @@ function PlantsList({showCareGuide}) {
         setPlantsData(plantsDataNew);
     }
     
+    if(hasErrored === true) {
+        return (
+            <div className="danger">
+                Error: <b>Loading Plants data failed {error}</b>
+            </div>
+        )
+    }
+    
     return(
         <div className="container plants-list">
-            <div className="row">
-                {plantsData.map(function (plantInfo) {
-                    return (
-                        <Plant 
-                            key={plantInfo.id}
-                            plantInfo = {plantInfo}
-                            showCareGuide={showCareGuide}
-                            onAddToMyGardenToggle={() => {onAddToMyGardenToggle(plantInfo.id)}}
-                        />
-                    )})
-                }
-            </div>
+            <ReactPlaceholder 
+                type="media"
+                rows={15}
+                className="plantslist-placeholder"
+                ready={isLoading===false}
+            >
+                <div className="row">
+                    {plantsData.map(function (plantInfo) {
+                        return (
+                            <Plant 
+                                key={plantInfo.id}
+                                plantInfo = {plantInfo}
+                                showCareGuide={showCareGuide}
+                                onAddToMyGardenToggle={() => {onAddToMyGardenToggle(plantInfo.id)}}
+                            />
+                        )})
+                    }
+                </div>
+            </ReactPlaceholder>
         </div>
     )
 }
